557f9246095bffbe2cfb41591142d3c24846838a
Justin Pedersen <elessards@gmail.com>
2025-09-03 19:08:27 -0700

    finished hw01 question 1, added .gitignore

---
+++ school/matlab/asen5307/asen5307_hw01_q02.m
@@ -0,0 +1,14 @@
+% HOMEWORK #01
+% Author:   Justin Pedersen
+% Date:     September 3rd, 2025
+% Class:    ASEN 5307 Engineering Data Analysis Methods
+
+%% PREAMBLE
+
+clear; close all; clc;
+fprintf("ASEN 5037 HOMEWORK 01 QUESTION 02\n")
+
+%% LOAD DATA
+
+% load raw data from .mat
+data_raw = load('data/boulder_precip.mat');
